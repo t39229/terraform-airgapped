e2a861b44cc8e795ce0dd93a935dbf47772b5690
t39229 <t39163463@gmail.com>
2024-09-18 12:56:22 +0300

    Create master_nodes_pre_setup.sh

---
+++ terraform/files/master_nodes_pre_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Disable automatic updates
+cat << EOF | sudo tee -a /etc/apt/apt.conf.d/20auto-upgrades
+APT::Periodic::Update-Package-Lists "0";
+APT::Periodic::Download-Upgradeable-Packages "0";
+APT::Periodic::AutocleanInterval "0";
+APT::Periodic::Unattended-Upgrade "0";
+EOF
+
+mkdir -p /mnt/irisplus-data
+chmod 777 /mnt/irisplus-data
+
+#Create user and add sudo
+useradd -m -s /bin/bash bastion && usermod -aG sudo bastion
+echo "bastion:bastion" | chpasswd
+sudo echo 'bastion  ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/00-no-pass-sudo
+sudo chmod 0440 /etc/sudoers.d/00-no-pass-sudo
+
+
+#Allow for password login
+sed -i -E 's/#?PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+systemctl restart sshd
